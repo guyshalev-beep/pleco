0a643662a097afb3de6cd77384395a8fdbde78e3
guy <guy@shalev-family.com>
2021-11-14 22:57:56 +0200

    complete mongo handler

--- pleco_target/kubectl_helper.py
+++ pleco_target/kubectl_helper.py
@@ -1,12 +1,13 @@
 import os
 import time
 
+
 class KubectlHelper(object):
     @staticmethod
     def getLoadBalancerIP(context, ns, resource_lb_name):
         p = os.popen("kubectl --context %s -n %s get service %s -o jsonpath='{.status.loadBalancer.ingress[0].ip}'"
                      % (context, ns, resource_lb_name))
-        return p.read()[:-1]
+        return p.read()
 
     @staticmethod
     def getNodePortIP(context, ns, resource_np_name):
@@ -18,18 +19,27 @@ class KubectlHelper(object):
     def waitForStatefulsetCondition(context, ns, resource_name):
         os.system("kubectl --context %s -n %s wait --for=condition=ready pod --timeout=60s -l "
                   "statefulset.kubernetes.io/pod-name=%s-0 "
-                  %(context,ns, resource_name))
+                  % (context, ns, resource_name))
 
     @staticmethod
     def waitForLBServiceCondition(context, ns, resource_name, timeout):
-        print("Waiting (for %is)for LoadBalancer Service %s"%(timeout, resource_name), sep=' ', end='', flush=True)
-        ip = KubectlHelper.getLoadBalancerIP(context,ns,resource_name)
+        print("Waiting (for %is)for LoadBalancer Service %s" % (timeout, resource_name), sep=' ', end='', flush=True)
+        ip = ""
         for i in range(timeout):
+            ip = KubectlHelper.getLoadBalancerIP(context, ns, resource_name)
+            if (ip != ""):
+                print(ip)
+                return
             print(".", sep=' ', end='', flush=True)
             time.sleep(1)
-        print (ip)
+        print(ip)
 
     @staticmethod
     def applyYaml(context, ns, file_name):
         os.system("kubectl --context %s -n %s apply -f %s"
                   % (context, ns, file_name))
+
+    @staticmethod
+    def deleteStatefulset(context, ns, name):
+        os.system("kubectl --context %s -n %s delete statefulset %s"
+                  % (context, ns, name))
--- pleco_target/service_handler.py
+++ pleco_target/service_handler.py
@@ -34,22 +34,61 @@ def handle_leap_to_new_cluster(sources_doc, step_doc):
                     client_port=str(leader_source['port']), client_token=leader_source['token']))
     print(serviceRes)
 
+
+
 def handle_standalone(sources_doc, step_doc):
     # deploy to follower source
-    source = [s for s in sources_doc if s['name'] == "follower_source"][0]
+    follower_source = [s for s in sources_doc if s['name'] == "follower_source"][0]
+    follower_client = K8sGWStub(grpc.insecure_channel("%s:50051" % follower_source['externalIP']))
+    leader_source = [s for s in sources_doc if s['name'] == "leader_source"][0]
+    leader_client = K8sGWStub(grpc.insecure_channel("%s:50051" % leader_source['externalIP']))
     yaml = step_doc['resource']['body']
-    follower_client = K8sGWStub(grpc.insecure_channel("%s:50051" % source['externalIP']))
     ns = step_doc['resource']['namespace']
     resource_name = step_doc['resource']['name']
-    print("start handle_standalone for:%s to follower:%s" % (resource_name,source['externalIP']))
+    if ("target_cluster" in step_doc['resource']):
+        target_cluster_name = step_doc['resource']['target_cluster']
+    else:
+        target_cluster_name = "follower"
     # Create on follower
-    service_res = follower_client.ApplyService(
+    if(target_cluster_name == "leader"):
+        client = leader_client
+        source = leader_source
+    else:
+        client = follower_client
+        source = follower_source
+    print("start handle_standalone for:%s to %s:%s" % (resource_name,target_cluster_name,source['externalIP']))
+    service_res = client.ApplyService(
         K8sGWRequest(body=str(yaml), namespace=ns,
                      client_host=source['api_server'],
                      client_port=str(source['port']),
                      client_token=source['token']))
     print(service_res)
   #  os.system("kubectl -n %s wait --for=condition=available deployment %s --timeout=5m" % (ns, resource_name))
+
+def handle_standalone_delete(sources_doc, step_doc):
+    follower_source = [s for s in sources_doc if s['name'] == "follower_source"][0]
+    follower_client = K8sGWStub(grpc.insecure_channel("%s:50051" % follower_source['externalIP']))
+    leader_source = [s for s in sources_doc if s['name'] == "leader_source"][0]
+    leader_client = K8sGWStub(grpc.insecure_channel("%s:50051" % leader_source['externalIP']))
+    ns = step_doc['resource']['namespace']
+    resource_name = step_doc['resource']['name']
+    if ("target_cluster" in step_doc['resource']):
+        target_cluster_name = step_doc['resource']['target_cluster']
+    else:
+        target_cluster_name = "follower"
+    # Create on follower
+    if(target_cluster_name == "leader"):
+        client = leader_client
+        source = leader_source
+    else:
+        client = follower_client
+        source = follower_source
+    print("start handle_standalone_delete for:%s to %s:%s" % (resource_name,target_cluster_name,source['externalIP']))
+    serviceRes = leader_client.DeleteService(
+        K8sGWRequest(resourceName=resource_name, namespace=ns, client_host=source['api_server'],
+                    client_port=str(source['port']), client_token=source['token']))
+    print(serviceRes)
+
 
 class ServiceHandler(object):
     def __init__(self):
@@ -63,4 +102,6 @@ class ServiceHandler(object):
             return handle_leap_to_new_cluster(sources_doc, step_doc)
         if method == 'standalone':
             return handle_standalone(sources_doc, step_doc)
+        if method == 'standalone_delete':
+            return handle_standalone_delete(sources_doc, step_doc)
         return None
